31b65012eb8df16b6a2d80826a5a585ac9de6981
legoracer97 <82517854+legoracer97@users.noreply.github.com>
2021-04-14 14:01:17 +0100

    initial commit

---
+++ overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "nec"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1618338857",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "The National Exhibition Centre",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "nec",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "nec",
+            "last_rendertime": 1618338455,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -24,
+                5,
+                -1
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -24,
+                5,
+                -1
+            ],
+            "north_direction": 2
+        }
+    ]
+};
